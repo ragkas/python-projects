99e0a92680dc9c0218dc2e0a23756a6dc5200c9b
ragkas <150574253+ragkas@users.noreply.github.com>
2023-11-14 02:27:25 -0500

    Tip calculator

---
+++ tip_calculator.py
@@ -0,0 +1,16 @@
+name1 = "Angela Yu"
+name2 = "Jack Bauer"
+
+l1 = list(name1.lower())
+l2 = list(name2.lower())
+t = name1.count("t") + name2.count("t")
+r = name1.count("r") + name2.count("r")
+u = name1.count("u") + name2.count("u")
+e1 = name1.count("e") + name2.count("e")
+trueL = t + r + u + e1
+l = name1.count("l") + name2.count("l")
+o = name1.count("o") + name2.count("o")
+v = name1.count("v") + name2.count("v")
+e2 = name1.count("e") + name2.count("e")
+loveL = l + o + v + e2
+print("scoe")
